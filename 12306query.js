bf5b3c159f99a727e035ca779656b86f4b3d7a1d
youer <673125641@qq.com>
2018-02-06 11:53:10 +0800

    Update 12306query.js
    add more condition for detect timeout or error

--- 12306query.js
+++ 12306query.js
@@ -10,15 +10,29 @@
 
 (function() {
     'use strict';
+    // change default autoSearchTime to 2000ms
     window.autoSearchTime = 2000;
+
+    // set interval for 2s
     setInterval(function(){
+        // find timeout div element
         var noTicket = $("#no_filter_ticket_6");
+        // find query button
+        var query = $("#query_ticket");
+
+        // if the element display was black, click query button twice
         if (noTicket.css("display") == "block") {
             console.log("检测到刷新超时。");
-            var query = $("#query_ticket");
+
+            // click twice
             query.click();
             query.click();
             console.log("点击刷新按钮成功。");
+        // if no train display, click query button twice
+        } else if ( $("#train_num_0").length < 1){
+            // click twice
+            query.click();
+            query.click();
         } else {
             console.log("未检测到...");
         }
