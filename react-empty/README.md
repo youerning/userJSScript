96144c69925aafe236952c394c425341518577c5
root <673125641@qq.com>
2018-07-31 11:18:09 +0800

    empty react project with react-router

---
+++ react-empty/README.md
@@ -0,0 +1,51 @@
+# admin-fe
+
+#### 项目介绍
+react 后台管理系统
+
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1. 安装依赖     
+```
+# 安装依赖包
+yarn install
+```
+2. 打包
+```
+#普通打包, 不适用于生产环境
+yarn run dist
+# 生产环境打包
+yarn run disp
+```
+3. 测试
+```
+#启动webpack-dev-server测试服务器用于测试
+yarn run dev
+```
+
+#### 使用说明
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 参与贡献
+
+1. Fork 本项目
+2. 新建 Feat_xxx 分支
+3. 提交代码
+4. 新建 Pull Request
+
+
+#### 码云特技
+
+1. 使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
+2. 码云官方博客 [blog.gitee.com](https://blog.gitee.com)
+3. 你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解码云上的优秀开源项目
+4. [GVP](https://gitee.com/gvp) 全称是码云最有价值开源项目，是码云综合评定出的优秀开源项目
+5. 码云官方提供的使用手册 [http://git.mydoc.io/](http://git.mydoc.io/)
+6. 码云封面人物是一档用来展示码云会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
--- react-empty/src/app.jsx
+++ react-empty/src/app.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React from "react"; 
 import ReactDOM from "react-dom";
-import "./index.css";
-import "./index.scss";
 import "font-awesome/css/font-awesome.min.css";
 
+let name = "youer";
+let nameSet = ["youer", "chord", "tom"]
+let flag = false;
+
+
+class ChildA extends React.Component {
+    render() {
+        return <div>Child A</div>
+    }
+}
+
+class ChildB extends React.Component {
+    render() {
+        return <div>Child B</div>
+    }
+}
+
+
+class Wrapper extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>{this.props.children}</div>
+    }
+}
 
 ReactDOM.render(
-    <div>
-        <i className="fa fa-address-book"></i>
-        <h1>hello world from React</h1>    
-    </div>
+    <Wrapper>
+        <ChildB></ChildB>
+        <ChildA></ChildA>
+    </Wrapper>
     ,
     document.getElementById('app')
 );
+ 
